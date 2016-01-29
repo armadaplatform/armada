b300e70ef6fc3d0bf6d8c826b9e3e0dac941835d
Filip Wójciak <fwojciak@ganymede.eu>
2016-01-29 09:32:12 +0100

    Install wget in microservice.

--- docker-containers/microservice/scripts/install_packages.sh
+++ docker-containers/microservice/scripts/install_packages.sh
@@ -1,8 +1,8 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 apt-get update
 apt-get upgrade -y
-apt-get install -y supervisor python python-dev python-pip curl mc software-properties-common
+apt-get install -y supervisor python python-dev python-pip curl mc software-properties-common wget
 add-apt-repository -y ppa:vbernat/haproxy-1.5
 apt-get update
 apt-get install -y haproxy
