f000e0d8fe61b6a5e038c0dca82b58e29df0841a
Michal Dawid <michal.dawid@gamedesire.eu>
2016-07-22 13:13:11 +0200

    command armada list now uses REST api

--- armada_backend/api_list.py
+++ armada_backend/api_list.py
@@ -1,4 +1,5 @@
 import web
+import re
 
 import api_base
 from armada_command.consul import kv
@@ -14,15 +15,20 @@ class List(api_base.ApiCommand):
 
     def GET(self):
         try:
-            get_args = web.input(local=False, microservice_name=None)
+            get_args = web.input(local=False, microservice_name=None, env=None, app_id=None)
             filter_local = bool(get_args.local)
             filter_microservice_name = get_args.microservice_name
+            filter_env = get_args.env
+            filter_app_id = get_args.app_id
 
             if filter_local:
                 local_microservices_ids = set(consul_query('agent/services').keys())
 
             if filter_microservice_name:
                 microservices_names = [filter_microservice_name]
+                if '*' in filter_microservice_name:
+                    names = list(consul_query('catalog/services').keys())
+                    microservices_names = self.__filter_names(filter_microservice_name, names)
             else:
                 microservices_names = list(consul_query('catalog/services').keys())
 
@@ -47,8 +53,12 @@ class List(api_base.ApiCommand):
                     container_id = microservice_id.split(':')[0]
                     microservice_tags = instance['Service']['Tags'] or []
                     microservice_tags_dict = self.__create_dict_from_tags(microservice_tags)
+                    microservice_tags_set = set(microservice_tags)
 
-                    if not filter_local or microservice_id in local_microservices_ids:
+                    matches_env = (filter_env is None) or ('env:' + filter_env in microservice_tags_set)
+                    matches_app_id = (filter_app_id is None) or ('app_id:' + filter_app_id in microservice_tags_set)
+
+                    if matches_env and matches_app_id and (not filter_local or microservice_id in local_microservices_ids):
                         microservice_address = microservice_ip + ':' + microservice_port
                         try:
                             microservice_start_timestamp = kv.get("start_timestamp/" + container_id)
@@ -68,3 +78,17 @@ class List(api_base.ApiCommand):
             return self.status_ok({'result': result})
         except Exception as e:
             return self.status_exception("Cannot get the list of services.", e)
+
+    @staticmethod
+    def __filter_names(filter, names):
+        expr = filter[1:-1]
+        if filter[0] != '*':
+            expr = '^' + filter[0:-1]
+        elif filter[-1] != '*':
+            expr = filter[1:] + '$'
+        ce = re.compile(expr)
+        service_names = []
+        for i in names:
+            if ce.search(i) is not None:
+                service_names.append(i)
+        return service_names
