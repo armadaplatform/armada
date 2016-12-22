2e5f73d90173b72721854e4cf8f57166deb5167b
Adam Marszałek <amarszalek@ganymede.eu>
2016-12-22 14:59:09 +0100

    Base image and template for web services implemented using .NET Core 1.0

---
+++ microservice_templates/microservice_dotnet_template/HelloWorldModule.cs
@@ -0,0 +1,11 @@
+using Nancy;
+namespace _MICROSERVICE_DOTNET_TEMPLATE_
+{
+        public class HomeModule : NancyModule
+    {
+        public HomeModule()
+        {
+            Get("/", args => "Hello, World!");
+        }
+    }
+}
