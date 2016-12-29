33b5e061a8beaa8262fafa088e9f34c2d62b22d7
Filip Wójciak <fwojciak@ganymede.eu>
2016-12-29 10:31:42 +0100

    Stub of ubuntu 16 in microservice.

--- docker-containers/microservice/scripts/install_packages.sh
+++ docker-containers/microservice/scripts/install_packages.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
 
-apt-get update
-apt-get upgrade -y
-apt-get install -y supervisor python python-dev python-pip curl mc software-properties-common wget gcc --no-install-recommends
-add-apt-repository -y ppa:vbernat/haproxy-1.6
-apt-get update
-apt-get install -y haproxy --no-install-recommends
-apt-get clean
-rm -rf /var/lib/apt/lists/*
+#apt-get update
+#apt-get upgrade -y
+#apt-get install -y supervisor python python-dev python-pip curl mc software-properties-common wget gcc --no-install-recommends
+#add-apt-repository -y ppa:vbernat/haproxy-1.6
+#apt-get update
+#apt-get install -y haproxy --no-install-recommends
+#apt-get clean
+#rm -rf /var/lib/apt/lists/*
 
 pip install -U pip
+pip install -U setuptools
 pip install -U docker-py web.py "requests==2.9.1"
