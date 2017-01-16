1c65d361b9ddf01075207e632de5dd8331c398ad
Filip Wójciak <fwojciak@ganymede.eu>
2017-01-16 13:16:59 +0100

    Fix exception message.

--- armada_backend/api_register.py
+++ armada_backend/api_register.py
@@ -6,5 +6,5 @@ class Register(api_base.ApiCommand):
         try:
             result = {}
         except Exception as e:
-            return self.status_exception('Could not get armada info.', e)
+            return self.status_exception('Could not register service.', e)
         return self.status_ok({'result': result})
