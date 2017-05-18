53c7e1f12e6825503830841206b11e5ef6504d8e
Krzysztof Indyk <kindyk@ganymede.eu>
2017-05-18 10:18:36 +0200

    don't run on import

--- armada_backend/clean_duplicated_saved_containers.py
+++ armada_backend/clean_duplicated_saved_containers.py
@@ -2,11 +2,17 @@ import filecmp
 import glob
 import os
 
-files = glob.glob('/opt/armada/saved_containers_backup/running_containers_parameters_????-??-??_??-??-??.json')
-previous = None
-for file in sorted(files):
-    if previous is not None:
-        equal = filecmp.cmp(previous, file)
-        if equal:
-            os.unlink(previous)
-    previous = file
+
+def main():
+    files = glob.glob('/opt/armada/saved_containers_backup/running_containers_parameters_????-??-??_??-??-??.json')
+    previous = None
+    for file in sorted(files):
+        if previous is not None:
+            equal = filecmp.cmp(previous, file)
+            if equal:
+                os.unlink(previous)
+        previous = file
+
+
+if __name__ == "__main__":
+    main()
