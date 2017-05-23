e05ea427330879729c027fecf766cc7145c8bce1
krise3k <krise3k@gmail.com>
2017-05-23 12:44:19 +0200

    Revert "Fix armada list when one of ships was renamed."
    This reverts commit 1d1b10c066fb85496a72f7263040da92dce2c1b8.

--- armada_backend/api_list.py
+++ armada_backend/api_list.py
@@ -11,113 +11,108 @@ from armada_command.consul.consul import consul_query
 
 
 class List(api_base.ApiCommand):
+    @staticmethod
+    def __create_dict_from_tags(tags):
+        if not tags:
+            return {}
+        return dict((tag.split(':', 1) + [None])[:2] for tag in tags)
+
     def GET(self):
         try:
             get_args = web.input(local=False, microservice_name=None, env=None, app_id=None)
-            filters = {
-                'filter_local': bool(get_args.local and strtobool(str(get_args.local))),
-                'filter_microservice_name': get_args.microservice_name,
-                'filter_env': get_args.env,
-                'filter_app_id': get_args.app_id
-            }
+            filter_local = bool(get_args.local and strtobool(str(get_args.local)))
+            filter_microservice_name = get_args.microservice_name
+            filter_env = get_args.env
+            filter_app_id = get_args.app_id
 
-            services_list = _get_services_list(**filters)
-            services_list.update(_get_running_services(**filters))
-            services_list = _choose_active_instances(services_list)
-            return self.status_ok({'result': services_list.values()})
+            services_list = _get_services_list(filter_microservice_name, filter_env, filter_app_id, filter_local)
+
+            if filter_local:
+                local_microservices_ids = set(consul_query('agent/services').keys())
+
+            if filter_microservice_name:
+                names = list(consul_query('catalog/services').keys())
+                microservices_names = fnmatch.filter(names, filter_microservice_name)
+            else:
+                microservices_names = list(consul_query('catalog/services').keys())
+
+            start_timestamps = kv.kv_get_recurse('start_timestamp/') or {}
+
+            single_active_instances = kv.kv_get_recurse('single_active_instance/')
+            if single_active_instances:
+                single_active_instances_list = single_active_instances.keys()
+            else:
+                single_active_instances_list = []
+
+            services_list_from_catalog = {}
+
+            for microservice_name in microservices_names:
+                if microservice_name == 'consul':
+                    continue
+
+                query = 'health/service/{microservice_name}'.format(**locals())
+                instances = consul_query(query)
+                for instance in instances:
+                    microservice_checks_statuses = set(check['Status'] for check in (instance['Checks'] or []))
+                    microservice_computed_status = '-'
+                    for possible_status in ['passing', 'warning', 'critical']:
+                        if possible_status in microservice_checks_statuses:
+                            microservice_computed_status = possible_status
+
+                    microservice_ip = instance['Node']['Address']
+                    microservice_port = str(instance['Service']['Port'])
+                    microservice_id = instance['Service']['ID']
+                    container_id = microservice_id.split(':')[0]
+                    microservice_tags = instance['Service']['Tags'] or []
+                    microservice_tags_dict = self.__create_dict_from_tags(microservice_tags)
+
+                    matches_env = (filter_env is None) or (filter_env == microservice_tags_dict.get('env'))
+                    matches_app_id = (filter_app_id is None) or (filter_app_id == microservice_tags_dict.get('app_id'))
+
+                    if (matches_env and matches_app_id and
+                            (not filter_local or microservice_id in local_microservices_ids)):
+                        microservice_address = microservice_ip + ':' + microservice_port
+                        microservice_start_timestamp = start_timestamps.get(container_id, None)
+                        single_active_instance = microservice_id in single_active_instances_list
+
+                        microservice_dict = {
+                            'name': microservice_name,
+                            'address': microservice_address,
+                            'microservice_id': microservice_id,
+                            'container_id': container_id,
+                            'status': microservice_computed_status,
+                            'tags': microservice_tags_dict,
+                            'start_timestamp': microservice_start_timestamp,
+                            'single_active_instance': single_active_instance,
+                        }
+                        services_list_from_catalog[microservice_id] = microservice_dict
+
+            result = services_list
+            result.update(services_list_from_catalog)
+            result = _choose_active_instances(result)
+            return self.status_ok({'result': result.values()})
         except Exception as e:
             return self.status_exception("Cannot get the list of services.", e)
 
 
-def __create_dict_from_tags(tags):
-    if not tags:
-        return {}
-    return dict((tag.split(':', 1) + [None])[:2] for tag in tags)
-
-
 def _get_services_list(filter_microservice_name, filter_env, filter_app_id, filter_local):
-    consul_key = 'containers_parameters_list'
+    ships_key = 'ships'
     if filter_local:
-        consul_key = '{}/{}'.format(consul_key, get_ship_name())
+        ships_key = "{}/{}".format(ships_key, get_ship_name())
 
-    services_by_ship = kv.kv_get_recurse(consul_key)
+    services_dict = kv.kv_get_recurse(ships_key)
 
-    if not services_by_ship:
+    if not services_dict:
         return {}
 
-    result = {}
-    for services_dict in services_by_ship.values():
-        result.update(_parse_single_ship(services_dict, filter_microservice_name, filter_env, filter_app_id))
-
-    return result
-
-
-def _get_running_services(filter_microservice_name, filter_env, filter_app_id, filter_local):
-    if filter_local:
-        local_microservices_ids = set(consul_query('agent/services').keys())
-    if filter_microservice_name:
-        names = list(consul_query('catalog/services').keys())
-        microservices_names = fnmatch.filter(names, filter_microservice_name)
-    else:
-        microservices_names = list(consul_query('catalog/services').keys())
-    start_timestamps = kv.kv_get_recurse('start_timestamp/') or {}
-    single_active_instances = kv.kv_get_recurse('single_active_instance/')
-    if single_active_instances:
-        single_active_instances_list = single_active_instances.keys()
-    else:
-        single_active_instances_list = []
-    services_list_from_catalog = {}
-    for microservice_name in microservices_names:
-        if microservice_name == 'consul':
-            continue
-
-        query = 'health/service/{microservice_name}'.format(**locals())
-        instances = consul_query(query)
-        for instance in instances:
-            microservice_checks_statuses = set(check['Status'] for check in (instance['Checks'] or []))
-            microservice_computed_status = '-'
-            for possible_status in ['passing', 'warning', 'critical']:
-                if possible_status in microservice_checks_statuses:
-                    microservice_computed_status = possible_status
-
-            microservice_ip = instance['Node']['Address']
-            microservice_port = str(instance['Service']['Port'])
-            microservice_id = instance['Service']['ID']
-            container_id = microservice_id.split(':')[0]
-            microservice_tags = instance['Service']['Tags'] or []
-            microservice_tags_dict = __create_dict_from_tags(microservice_tags)
-
-            matches_env = (filter_env is None) or (filter_env == microservice_tags_dict.get('env'))
-            matches_app_id = (filter_app_id is None) or (filter_app_id == microservice_tags_dict.get('app_id'))
-
-            if (matches_env and matches_app_id and
-                    (not filter_local or microservice_id in local_microservices_ids)):
-                microservice_address = microservice_ip + ':' + microservice_port
-                microservice_start_timestamp = start_timestamps.get(container_id, None)
-                single_active_instance = microservice_id in single_active_instances_list
-
-                microservice_dict = {
-                    'name': microservice_name,
-                    'address': microservice_address,
-                    'microservice_id': microservice_id,
-                    'container_id': container_id,
-                    'status': microservice_computed_status,
-                    'tags': microservice_tags_dict,
-                    'start_timestamp': microservice_start_timestamp,
-                    'single_active_instance': single_active_instance,
-                }
-                services_list_from_catalog[microservice_id] = microservice_dict
-    return services_list_from_catalog
-
-
-def _parse_single_ship(services_dict, filter_microservice_name, filter_env, filter_app_id):
     services_list = services_dict.keys()
+
     result = {}
     if not services_list:
         return result
 
     if filter_microservice_name:
-        services_list = fnmatch.filter(services_list, 'ships/*/service/{}/*'.format(filter_microservice_name))
+        services_list = fnmatch.filter(services_list, '*/service/{}/*'.format(filter_microservice_name))
 
     for service in services_list:
         service_dict = services_dict[service]
@@ -153,7 +148,6 @@ def _parse_single_ship(services_dict, filter_microservice_name, filter_env, filt
                 'single_active_instance': single_active_instance,
             }
             result[microservice_id] = microservice_dict
-
     return result
 
 
