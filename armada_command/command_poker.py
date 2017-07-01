a614e6d630cccecfa34ac6f96199e0ae8bb8138a
mdawid92 <michal.dawid92@gmail.com>
2017-07-01 22:31:17 +0200

    added so sick feature

--- armada_command/command_poker.py
+++ armada_command/command_poker.py
@@ -1,6 +1,7 @@
 import random
 import sys
 import wave
+import pyaudio
 
 from PIL import Image
 
@@ -10,33 +11,39 @@ CHUNK = 1024
 
 
 def command_poker(args):
+    im = Image.open('/opt/armada/armada_command/utils/so_sick/poker.png')
+    im = im.resize((120, 80))
     so_sick = '/opt/armada/armada_command/utils/so_sick/{}.wav'.format(random.randint(1, 11))
     wf = wave.open(so_sick, 'rb')
-    # p = pyaudio.PyAudio()
-    #
-    # stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
-    #                 channels=wf.getnchannels(),
-    #                 rate=wf.getframerate(),
-    #                 output=True)
-    #
-    # data = wf.readframes(CHUNK)
-    #
-    # while data != '':
-    #     stream.write(data)
-    #     data = wf.readframes(CHUNK)
-    #
-    # stream.stop_stream()
-    # stream.close()
-    #
-    # p.terminate()
+    p = pyaudio.PyAudio()
 
-    im = Image.open('/opt/armada/armada_command/utils/so_sick/poker.png')
-    im = im.resize((90, 60))
-    for y in range(im.size[1]):
-        for x in range(im.size[0]):
-            p = im.getpixel((x, y))
-            h = "%2x%2x%2x" % (p[0], p[1], p[2])
-            short, rgb = image_to_ansi.rgb2short(h)
-            sys.stdout.write("\033[48;5;%sm " % short)
-        sys.stdout.write("\033[0m\n")
+    stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
+                    channels=wf.getnchannels(),
+                    rate=wf.getframerate(),
+                    output=True)
+
+    data = wf.readframes(CHUNK)
+    y = 0
+    while data != '':
+        if y < im.size[1]:
+            draw_row(im, y)
+            y += 1
+        stream.write(data)
+        data = wf.readframes(CHUNK)
+    while y < im.size[1]:
+        draw_row(im, y)
+        y += 1
     sys.stdout.write("\n")
+    stream.stop_stream()
+    stream.close()
+
+    p.terminate()
+
+
+def draw_row(im, y):
+    for x in range(im.size[0]):
+        pixel = im.getpixel((x, y))
+        h = "%2x%2x%2x" % (pixel[0], pixel[1], pixel[2])
+        short, rgb = image_to_ansi.rgb2short(h)
+        sys.stdout.write("\033[48;5;%sm " % short)
+    sys.stdout.write("\033[0m\n")
