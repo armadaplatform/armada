e3bb56b9bafd36517f88470b7c37c6bb2ab3da74
krise3k <krise3k@gmail.com>
2017-10-13 23:18:21 +0200

    Use --build-arg for nodejs microservice

--- docker-containers/microservice_node_template/build_nodejs_microservices.sh
+++ docker-containers/microservice_node_template/build_nodejs_microservices.sh
@@ -5,10 +5,7 @@ VERSIONS_TO_BUILD=(4 6 8)
 #workdir to script directory
 cd "$(dirname "${BASH_SOURCE[0]}")"
 
-docker pull dockyard.armada.sh/microservice
-docker tag dockyard.armada.sh/microservice  microservice
-
 for NODE_VERSION in "${VERSIONS_TO_BUILD[@]}"
 do
-    docker build -t "microservice_node${NODE_VERSION}" --build-arg "NODE_VERSION=${NODE_VERSION}" ./
+    armada build --build-arg "NODE_VERSION=${NODE_VERSION}" "microservice_node${NODE_VERSION}"
 done
