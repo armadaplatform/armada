1b169e9e79c12170a8693982c1e4b51993e0f613
Szymon Kempny <szymon.kempny@gamedesire.eu>
2018-02-01 16:25:12 +0100

    refactoring

--- docker-containers/microservice/build_package.sh
+++ docker-containers/microservice/build_package.sh
@@ -15,4 +15,4 @@ fi
 docker build --rm -t "${SERVICE_NAME}" -f packaging/Dockerfile ./packaging
 docker run --rm -t -v "$(pwd)/packaging:/opt/microservice" "${SERVICE_NAME}" --version="${PACKAGE_VERSION}"
 
-mv -f $(pwd)/packaging/armada-microservice_${PACKAGE_VERSION}_amd64.deb $(pwd)/
+mv -f "$(pwd)/packaging/armada-microservice_${PACKAGE_VERSION}_amd64.deb" "$(pwd)/"
