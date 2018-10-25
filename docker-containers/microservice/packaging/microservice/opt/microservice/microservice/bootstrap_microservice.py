ef20c561c070685c8499364eb816e41e3ffaa9be
fwojciak <fwojciak@users.noreply.github.com>
2018-10-25 10:29:05 +0200

    Fix config path (#220)
    * Fix generating `CONFIG_PATH` on renamed microservices.
    
    * refactor

--- docker-containers/microservice/packaging/microservice/opt/microservice/microservice/bootstrap_microservice.py
+++ docker-containers/microservice/packaging/microservice/opt/microservice/microservice/bootstrap_microservice.py
@@ -35,6 +35,12 @@ def _nesting_level(path):
     return path.rstrip('/').count('/')
 
 
+def _generate_config_full_path(base_path, config_dir, config_dirs_combinations):
+    image_config_dirs = [os.path.join(base_path, config_dir, path) for path in config_dirs_combinations]
+    image_config_dirs.sort(key=_nesting_level, reverse=True)
+    return image_config_dirs
+
+
 def main():
     if "CONFIG_DIR" in os.environ:
         service_path = os.path.join("/opt", os.environ["MICROSERVICE_NAME"])
@@ -43,10 +49,14 @@ def main():
         microservice_app_id = os.environ.get("MICROSERVICE_APP_ID", '')
 
         config_dirs_combinations = set(_get_all_parent_dirs_with_combinations(microservice_env, microservice_app_id))
+        service_config_dirs_full_paths = _generate_config_full_path(service_path, config_dir, config_dirs_combinations)
 
-        config_dirs_full_paths = [os.path.join(service_path, config_dir, path) for path in config_dirs_combinations]
-        config_dirs_full_paths.sort(key=_nesting_level, reverse=True)
-        config_dirs_existing_paths = list(filter(os.path.isdir, config_dirs_full_paths))
+        if os.environ.get("IMAGE_NAME") != os.environ["MICROSERVICE_NAME"]:
+            image_path = os.path.join("/opt", os.environ["IMAGE_NAME"])
+            image_config_dirs = _generate_config_full_path(image_path, config_dir, config_dirs_combinations)
+            service_config_dirs_full_paths.extend(image_config_dirs)
+
+        config_dirs_existing_paths = list(filter(os.path.isdir, service_config_dirs_full_paths))
 
         local_config_path = os.pathsep.join(config_dirs_existing_paths)
 
