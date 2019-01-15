ba35f15673c0fb0a291bdb003b039b194d3cde29
firesoft <firesoft1983@gmail.com>
2019-01-15 14:27:39 +0100

    update nodejs.sh in microservice_node_template
    increase max_old_space_size to 384mb, remove "--trace_gc" param

--- docker-containers/microservice_node_template/src/nodejs.sh
+++ docker-containers/microservice_node_template/src/nodejs.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 set -e
-exec "/usr/bin/nodejs_bin" "--max_old_space_size=${MAX_OLD_SPACE_SIZE_MB:-256}" --trace_gc "$@"
+exec "/usr/bin/nodejs_bin" "--max_old_space_size=${MAX_OLD_SPACE_SIZE_MB:-384}" "$@"
