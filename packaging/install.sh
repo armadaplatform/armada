61ce481a6f5acceda14826cfe5c9595ed1593ed0
Artur Łabuda <artur.labuda@gamedesire.eu>
2021-01-20 16:47:41 +0100

    Changes in armada/packaging

--- packaging/install.sh
+++ packaging/install.sh
@@ -48,23 +48,15 @@ if [ ! -d /var/log/armada ]; then
 fi
 
 
-POSSIBLE_PIP_COMMANDS=( 'pip2.7' 'pip-2.7' 'pip2' 'pip' )
-for PIP_COMMAND in "${POSSIBLE_PIP_COMMANDS[@]}"
-do
-    if command_exists "${PIP_COMMAND}"; then
-        pip="${PIP_COMMAND}"
-        break
-    fi
-done
-
-
-$sh_c "$pip install -r /opt/armada/armada_command_requirements.txt 2> /dev/null"
+$sh_c "python3 -m pip install -r /opt/armada/armada_command_requirements.txt 2> /dev/null"
 
 sudo bash -c ". /etc/bash_completion.d/armada"
 
 echo "Downloading armada image..."
-$sh_c "docker pull ${ARMADA_REPOSITORY}/armada:${ARMADA_VERSION}"
-$sh_c "docker tag ${ARMADA_REPOSITORY}/armada:${ARMADA_VERSION} armada:${ARMADA_VERSION}"
+echo "SKIPPED !!!!!!!!"
+# TODO:
+# $sh_c "docker pull ${ARMADA_REPOSITORY}/armada:${ARMADA_VERSION}"
+# $sh_c "docker tag ${ARMADA_REPOSITORY}/armada:${ARMADA_VERSION} armada:${ARMADA_VERSION}"
 
 if command_exists systemctl; then
     start_using_systemd
