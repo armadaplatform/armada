46d98bcdb34a279a00a52b024be46e22471f82e3
Bartosz Borysewicz <bartosz.borysewicz@gamedesire.com>
2025-11-14 08:33:31 +0100

    added --no-cache to rebuild micreoservice and microservice_php images

--- docker-containers/microservice/build_package.sh
+++ docker-containers/microservice/build_package.sh
@@ -12,7 +12,7 @@ if [[ ! -n ${PACKAGE_VERSION} ]]; then
     PACKAGE_VERSION=$(grep 'ENV MICROSERVICE_VERSION' Dockerfile | awk '{ print $3 }'  | tr -d '[[:space:]]')
 fi
 
-docker build --rm -t "${SERVICE_NAME}" -f packaging/Dockerfile ./packaging
+docker build --rm --no-cache -t "${SERVICE_NAME}" -f packaging/Dockerfile ./packaging
 docker run --rm -t -v "$(pwd)/packaging:/opt/microservice" "${SERVICE_NAME}" --version="${PACKAGE_VERSION}"
 
 mv -f "$(pwd)/packaging/armada-microservice_${PACKAGE_VERSION}_amd64.deb" "$(pwd)/"
